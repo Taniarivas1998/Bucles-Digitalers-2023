beb7fec1baa1fc9444261cf4edf10fc0eb75ae7d
Tania Rivas <taniarivas206@gmail.com>
2023-09-14 21:54:10 -0300

    nuevo js de for

---
+++ js/do-while.js
@@ -0,0 +1,16 @@
+let edadCliente
+
+do {
+
+    edadCliente = parseInt(prompt("Ingrese edad de cliente"));
+
+} while( isNaN(edadCliente) || edadCliente > 120 || edadCliente  <= 0   )
+
+
+
+// while( isNaN(edadCliente) || edadCliente > 120 || edadCliente  <= 0   ) {
+//     // Le vuelvo a pedir que haga un prompt
+//     edadCliente = parseInt(prompt("Ingrese edad de cliente"));
+// }
+
+console.log(edadCliente)
---
+++ js/for.js
@@ -0,0 +1,39 @@
+/*let alumno;
+let alumnosDelCurso=[];
+//Cuando la persona ingrese como nombre de alumno "exit"
+
+do{
+    alumno= prompt("Ingrese el nombre del alumno")
+    if(alumno!=="exit")alumnosDelCurso.push(alumno)
+}
+
+while(alumno!=="exit")*/
+
+/*let contador=1
+while(contador<==100){
+    //contador++
+}*/
+
+/*for(inicializar; condicion; valorAmodificar){
+}*/
+
+/*for(let i=1;  i<=100; i++){
+
+    console.log ('Iteracion numero ${i}')
+}*/
+
+const alumnosCurso=["Pedro","Maria","Miguel","Ana","Oli","Matias","Daniela","Lili","Fernando"]
+
+/*for (let i=0; i<alumnosCurso.length; i++){
+
+    console.log(alumnosCurso[i])
+}
+*/
+
+for(let alumno of alumnosCurso){
+
+console.log(alumno)
+
+}
+
+alumnosCurso.push("John")
